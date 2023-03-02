ab40a4973ca46ad1dc4dc09b44481c026d5ea1a1
ray491 <72789534+ray491@users.noreply.github.com>
2023-03-02 14:29:26 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// aariz
+function aariz() {
+    const userInput = prompt("Wachtwoord: ");
+    if (userInput.toLowerCase() === "123") {
+        window.open("/files/users/aariz.html")
+      }
+}
+// rahel
+function rahel() {
+    const userInput = prompt("Wachtwoord: ");
+    if (userInput.toLowerCase() === "Rahel123") {
+        window.open("/files/users/rahel.html");
+      }
+}
---
+++ js/users.js
@@ -0,0 +1,12 @@
+function film_1() {
+    window.open("/movies/spiderman.mkv")
+}
+function film_2() {
+    window.open("/movies/topgun_mav.mp4")
+}
+function film_3() {
+    window.open("/movies/topgun.mp4")
+}
+function film_4() {
+    window.open("")
+}
